dcf0fbec363a74b4ca01f6dcae4f9a18ba21d016
Terry Marr <terrymarr280@gmail.com>
2015-10-25 15:43:42 -0700

    initial styling home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,13 +10,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
-        assert_select "title", "Help | Ruby on Rails Tutorial"
+    assert_select "title", "Help | Ruby on Rails Tutorial"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-        assert_select "title", "About | Ruby on Rails Tutorial"
+    assert_select "title", "About | Ruby on Rails Tutorial"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial"
   end
 
 end
